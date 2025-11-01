9391932c80242f0309d51d9bcc45725ad9208d8f
Daniel <daniel.gil.romero24@gmail.com>
2025-11-01 14:24:00 +0100

    Played around with the tabs and its styles

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function RootLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ff6579a",
+        headerStyle: {
+          backgroundColor: '#24698a'
+        },
+        headerShadowVisible:false,
+        headerTintColor:'#fff',
+        tabBarStyle: {
+          backgroundColor: '#26597q'
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            ></Ionicons>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            ></Ionicons>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
